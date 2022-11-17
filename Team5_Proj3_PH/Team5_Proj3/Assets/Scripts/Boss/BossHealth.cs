5bc80a654b1ec143828a8bfa248d6fb5bb51ebf1
Sitelinear7 <owen0234@gmail.com>
2022-11-16 19:55:05 -0500

    Did things

--- Team5_Proj3_PH/Team5_Proj3/Assets/Scripts/Boss/BossHealth.cs
+++ Team5_Proj3_PH/Team5_Proj3/Assets/Scripts/Boss/BossHealth.cs
@@ -6,6 +6,7 @@ public class BossHealth : MonoBehaviour
 {
     [SerializeField] int bossHealth;
     [SerializeField] TMP_Text healthText;
+    [SerializeField] Transform EndZone;
 
     // Start is called before the first frame update
     void Start()
@@ -26,5 +27,9 @@ public class BossHealth : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    private IEnumerator Death()
+    {
+        yield return new WaitForSeconds(4);
     }
 }
